b587bd7f84908bf206f07f3dd86f41ef8cbb552c
AlexG <ramiro.a.garza@gmail.com>
2018-08-26 21:39:17 -0500

    updating material

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [
+    {
+    name:"Paulina",
+    photo: "/../paulina.jpeg",
+    scores:[
+        "4",
+        "5",
+        "3",
+        "4",
+        "4"
+    ]
+},
+{
+    name:"Alex",
+    photo: "/../Alex.jpg",
+    scores:[
+        "5",
+        "4",
+        "4",
+        "4",
+        "3"
+    ]
+}
+];
+
+module.exports = friends;
